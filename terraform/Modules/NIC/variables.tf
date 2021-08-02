103fb4a39859fadba95c394e0e7835908daf179b
djh82uk <djh82uk@gmail.com>
2021-08-02 16:10:37 +0100

    Fixed variable name

--- terraform/Modules/NIC/variables.tf
+++ terraform/Modules/NIC/variables.tf
@@ -2,7 +2,6 @@ variable "vm_nic_name" {}
 variable "location" {default = "west europe"}
 variable "resourcegroup" {}
 variable "addr_allocation" {default = "Dynamic"}
-variable "ip_name" {}
 variable "subnet_id" {}
-variable "public_ip" {default = NULL}
+variable "public_ip_address_id" {default = NULL}
 
