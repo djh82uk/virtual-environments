718f8939e7f86abe187026e2743a51c22c6907c4
djh82uk <djh82uk@gmail.com>
2021-08-02 16:43:24 +0100

    Fixes

--- terraform/Modules/NIC/nic.tf
+++ terraform/Modules/NIC/nic.tf
@@ -1,13 +1,14 @@
-resource "azurerm_network_interface" "VM1" {
+resource "azurerm_network_interface" "NIC" {
   name                = "${var.vm_nic_name}"
   location            = "${var.location}"
   resource_group_name = "${var.resourcegroup}"
 
   ip_configuration {
-    name                          = "${var.ip_name}"
+    name                          = "${var.ip_config_name}"
     subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "${var.addr_allocation}"
-    public_ip_address_id          = "${var.public_ip}"
+    public_ip_address_id          = "${var.public_ip_address_id}"
   }
 }
+
 
--- terraform/Modules/NIC/variables.tf
+++ terraform/Modules/NIC/variables.tf
@@ -4,4 +4,5 @@ variable "resourcegroup" {}
 variable "addr_allocation" {default = "Dynamic"}
 variable "subnet_id" {}
 variable "public_ip_address_id" {}
+variable "ip_config_name" {default = "ipconfig"}
 
